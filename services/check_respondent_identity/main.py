323d1ffc8b6075e5a0d7c7ec3a5cce01358671b8
Juan Cotrino <juan.cotrino@outlook.com>
2024-12-25 13:23:27 -0500

    Changed API implementation

--- services/check_respondent_identity/main.py
+++ services/check_respondent_identity/main.py
@@ -1,46 +1,30 @@
-# from goblet import Goblet, goblet_entrypoint
-# from flask import Request
+from flask import Flask, Request
 
-# import resources
-
-# app = Goblet(function_name="check_respondent_identity")
-# goblet_entrypoint(app)
-
-
-# @app.route('/check_respondent_identity', methods=['GET'])
-# def check_respondent_identity(request: Request):
-#     try:
-#         return {"hello": "world"}, 200
-#         # phone_number = request.args.get('phone_number')
-
-#         # if phone_number is None:
-#         #     message = "Phone number is required"
-#         #     app.log.info(message)
-#         #     return {"message": message}, 400
-
-#         # message = f"Phone number provided: {phone_number}. "
-#         # f"resources test: {resources.test_resource()}"
-#         # return {"message": message}, 200
-
-#     except Exception as e:
-#         # Create the failed response
-#         message = str(e)
-#         app.log.error(message)
-#         return {"message": message}, 400
-
-import os
-
-from flask import Flask
+import resources
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+def check_respondent_identity(request: Request):
+    try:
+        phone_number = request.args.get('phone_number')
+
+        if phone_number is None:
+            message = "Phone number is required"
+            app.log.info(message)
+            return {"message": message}, 400
+
+        message = f"Phone number provided: {phone_number}. "
+        f"resources test: {resources.test_resource()}"
+        return {"message": message}, 200
+
+    except Exception as e:
+        # Create the failed response
+        message = str(e)
+        app.log.error(message)
+        return {"message": message}, 400
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=False, host="0.0.0.0", port=8080)
